5b90454cbf52ddaa2de369c7c1660054f7e16e86
Mecnoble <147364518+Mecnoble@users.noreply.github.com>
2024-01-08 06:13:49 +0100

    Add files via upload

--- form-validation.js
+++ form-validation.js
@@ -1,45 +1,37 @@
-window.onload = function () {
-  console.log("DOM ready!");
-  document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    console.log("form submitted!");
-    const dateNaissance = document.getElementById("dateNaissance").value;
-    const dateNaissanceDate = new Date(dateNaissance);
-    const dateNaissanceTimestamp = dateNaissanceDate.getTime();
-    const prenom = document.getElementById("prenom").value;
-    const adresse = document.getElementById("adresse").value;
-    const email = document.getElementById("email").value;
+$(document).ready(function () {
+    $("#gps").on("click",function(){ //ici la géolocalisation nous donnera la latitude et la la longitude de l’utilsateur
+        getLocation(); //L'API Géolocalisation HTML5  est utilisée pour obtenir la position géographique d'un utilisateur 
+    });
+ 
+    $(document).keyup(function(e1) {
+   var nombreCaractere = $("#name").val().length;
+        var msg = nombreCaractere + ' car';
+    
+        $('#compteur1').text(msg);
+    
+    
+        var nombreCaractere = $("#firstname").val().length;
+        var msg = nombreCaractere + ' car';
+    
+        $('#compteur2').text(msg);
 
-    const nowTimestamp = Date.now();
+        // Le script qui devra calculer et afficher le nombre de mots et de caractères
+      
+        var nombreCaractere = $("#birth").val().length;
+        var msg = nombreCaractere + ' car';
+        $('#compteur3').text(msg);
 
-    if (dateNaissanceTimestamp >= nowTimestamp) {
-      alert("La date de naissance n'est pas compatible !");
-    }
+        var nombreCaractere4 = $("#adresse").val().length;
+        var msg = nombreCaractere + ' car';
+    
+        $('#compteur4').text(msg);
 
-    var myModal = new bootstrap.Modal(document.getElementById("myModal"));
-    myModal.show();
-    document.querySelector(".modal-body").innerHTML =
-      '<img src="https://maps.googleapis.com/maps/api/staticmap?markers=Paris&zoom=14&size=400x300&scale=2&key=VOTRE_CLE_API"/>';
-
-    const modalBody = myModal._element.querySelector(".modal-body");
-    modalBody.innerHTML = `<p>Vous êtes ${prenom} ${nom} et vous habitez :</p>
-                      <p>${adresse}</p>
-                      <img src="https://maps.googleapis.com/maps/api/staticmap?markers=${adresse}&zoom=14&size=400x300&scale=2&key=VOTRE_CLE_API"/>`;
-    if (
-      nom.length < 5 ||
-      prenom.length < 5 ||
-      email.length < 5 ||
-      adresse.length < 5
-    ) {
-      alert("Les champs texte doivent avoir au moins 5 caractères.");
-      return false;
-    }
-    return true;
-  });
-};
-
-function validateEmail(email) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
+        var nombreCaractere = $("#mail").val().length;
+        var msg = nombreCaractere + ' car';
+    
+        $('#compteur5').text(msg);
+      
+       
+     });
+});
+   
